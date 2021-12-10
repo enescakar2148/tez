a5d40ae5a4d6b9bc914eb05060ac017eca75ed45
Enes Çakar <e.52nes@hotmail.com>
2021-12-10 23:14:29 +0300

    aaa1

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -16,7 +16,7 @@ export default class Login extends Component {
                         <input type="password" placeholder="Enter Password" name="psw" className="input" />
                     </div>
 
-                    <button type="submit" className="login-btn">Giriş Yapppppp</button>
+                    <button type="submit" className="login-btn">Login</button>
 
                 </div>
             </div>
