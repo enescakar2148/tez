9523d58ba2a706a721abbffe2c50cbbc627deace
Tugce Akkaya <tgcakkaya1@gmail.com>
2021-12-11 19:46:56 +0300

    login frontend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import "../css/Login.css"
-
+import loginp from '../images/login1.png';
 
 export default class Login extends Component {
     render() {
         return (
             <div className="container">
-                <div className="inputs">
-                    <div className="labels1">
-                        <label for="uname" className="labels"> <b> Username </b> </label>
-                        <input type="text" placeholder="Enter Username" name="uname" className="input" />
-                    </div>
-                    <div className="labels2">
-                        <label for="psw" className="labels"><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" className="input" />
-                    </div>
-
-                    <button type="submit" className="login-btn">Login</button>
-
+                <div className="photo">
+                    <img src={loginp} />
                 </div>
-            </div>
-        )
+                <div className="content">
+                    
+                    <label className="label1" for="fname">E-mail </label>
+                    <input className="labelx" type="text" id="fname" name="fname" />
+                        <label for="lname">Şifre</label>
+                        <input type="text" className="labelx" id="lname" name="lname" />
+                        <div className='btn-b'>
+                        <label for="lname">Şifrenizi mi unuttunuz?</label>
+                        <button className="buttonx" > Giriş Yap </button>
+                        </div>
+               
+                        </div>
+
+
+
+                    </div>
+
+                        )
     }
 }
