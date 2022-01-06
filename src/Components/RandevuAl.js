b732b226aa4f4cc6f32b07b81d564f0c3beae33f
Tugce Akkaya <tgcakkaya1@gmail.com>
2022-01-07 01:48:08 +0300

    randevu al frontend tamamlandı

--- src/Components/RandevuAl.js
+++ src/Components/RandevuAl.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React ,  { useState, useCallback }from 'react'
+import CalendarP from "../images/calendar.png"
+import '@natscale/react-calendar/dist/main.css';
+import { Calendar } from '@natscale/react-calendar';
+import '../css/calendar.css';
+export default function RandevuAl() {
 
-export default class RandevuAl extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Randevu Al</h1>
-            </div>
-        )
-    }
+    const [value, setValue] = useState();
+
+    const onChange = useCallback(
+      (value) => {
+        setValue(value);
+      },
+      [setValue],
+    );
+  return (
+    <div className="calendar">
+            <div className="calendarp">
+        <img src={CalendarP}>
+    
+        </img>
+        </div>
+        <div className="calendar_t">
+      <Calendar value={value} onChange={onChange} />
+      <button className="calendar_btn">Randevu Al</button>
+      </div>
+    </div>
+  )
 }
